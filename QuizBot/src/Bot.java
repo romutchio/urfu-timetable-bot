a0885c0ca07c6e65f59e3c08a385c59c8bd617dc
vaspahomov <vas2142553@gmail.com>
2018-09-17 23:01:10 +0500

    some fix of Bot

--- QuizBot/src/Bot.java
+++ QuizBot/src/Bot.java
@@ -3,43 +3,42 @@ import jdk.jshell.spi.ExecutionControl;
 import java.util.Scanner;
 
 public class Bot {
-    private static void handleWelcome(Messages messages, Scanner in)
-    {
+    private static void handleWelcome(Messages messages, Scanner in) {
         System.out.println(messages.Welcome());
         String answer = in.nextLine();
 
-        if (answer.toLowerCase().equals("да")) {
-            System.out.println(messages.GroupQuestion());
-            mainLoop(messages, in);
-        }
+        switch (answer.toLowerCase()) {
+            case "да":
+                System.out.println(messages.GroupQuestion());
+                mainLoop(messages, in);
+                break;
+            case "нет":
+                System.out.println(messages.Decline());
+                break;
+            default:
+                System.out.println("Я вас не понял \nПопробуйте еще раз\n\n"); //перенести в ответы
 
-        else if (answer.toLowerCase().equals("нет")) {
-            System.out.println(messages.Decline());
-        } else {
-            System.out.println("Я вас не понял \nПопробуйте еще раз\n\n"); //перенести в ответы
-            handleWelcome(messages, in);
+                handleWelcome(messages, in);
+                break;
         }
     }
 
-    private static void mainLoop(Messages messages, Scanner in)
-    {
+    private static void mainLoop(Messages messages, Scanner in) {
         String groupName = in.nextLine();
         Group userGroup = AnswerValidator.RecognizeGroup(groupName);
         System.out.println(messages.GroupSelection(userGroup));
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Messages messages = new Messages();
         Scanner in = new Scanner(System.in);
 
         handleWelcome(messages, in);
 
-        while (true)
-        {
+        while (true) {
             String answer = in.nextLine();
 
-            switch (answer){
+            switch (answer) {
                 case "/start":
                     handleWelcome(messages, in);
                     break;
@@ -53,6 +52,14 @@ public class Bot {
                     System.out.println("Данная функция еще не реализованна ¯\\_(ツ)_/¯");
                     //throw new Exception();
                     break;
+
+                case "/exit":
+                    System.exit(1);
+                    break;
+                default:
+                    System.out.println("Я вас не понял, чтобы увидеть подсказки наберите: \"/help\"");
+                    break;
+
 
             }
         }
