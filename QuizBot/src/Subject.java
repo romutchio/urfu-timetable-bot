2fd8e506e222c482d1619b6459825504ecb8b526
insomnia* <budljanskiy@mail.ru>
2018-09-22 10:34:35 +0500

    TimeTable parser added

---
+++ QuizBot/src/Subject.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+public class Subject {
+    String weekday;
+    String lessonName;
+    String lessonStartTime;
+    String lessonEndTime;
+    String teacher;
+
+    public Subject(String weekday, String lessonName, String lessonStartTime, String lessonEndTime, String teacher)
+    {
+        this.weekday = weekday;
+        this.lessonName = lessonName;
+        this.lessonStartTime = lessonStartTime;
+        this.lessonEndTime = lessonEndTime;
+        this.teacher = teacher;
+    }
+}
